268834c7c34945e56be6b7d8edc58ca178e08cf2
anadi.sankritya <anadi.sankritya@starhealth.in>
2025-03-21 15:25:36 +0530

    adding default app config values

--- src/main/java/com/nft/app/config/AppConfiguration.java
+++ src/main/java/com/nft/app/config/AppConfiguration.java
@@ -1,15 +1,33 @@
 package com.nft.app.config;
 
+import com.nft.app.entity.AppConfig;
+import com.nft.app.repository.AppConfigRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@RequiredArgsConstructor
 public class AppConfiguration {
+
+  private final AppConfigRepository appConfigRepository;
+
+  @PostConstruct
+  public void init() {
+    AppConfig appConfig = new AppConfig();
+    appConfig.setOtpRequired(false);
+    appConfig.setReferralCodeMandatory(false);
+    appConfigRepository.deleteAll();
+    appConfigRepository.save(appConfig);
+  }
+
 
   @Bean
   public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
     return restTemplateBuilder.build();
   }
+
 }
