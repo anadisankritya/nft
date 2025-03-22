ad7b285f04f8b9fc3b9b1964dddab75ef6c66290
anadi.sankritya <anadi.sankritya@starhealth.in>
2025-03-22 14:41:37 +0530

    adding WalletController

---
+++ src/main/java/com/nft/app/controller/WalletController.java
@@ -0,0 +1,13 @@
+package com.nft.app.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/wallet")
+@RequiredArgsConstructor
+public class WalletController {
+
+}
