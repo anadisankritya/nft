2fc63b0b0ab2770b34dc66037cedd0624054a8fe
anadi.sankritya <anadi.sankritya@starhealth.in>
2025-03-22 15:05:29 +0530

    fix

--- src/main/java/com/nft/app/dto/request/UserRequest.java
+++ src/main/java/com/nft/app/dto/request/UserRequest.java
@@ -2,28 +2,27 @@ package com.nft.app.dto.request;
 
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 
 public record UserRequest(
 
     @NotEmpty(message = "username is required")
     String username,
 
-    @NotNull(message = "email is required")
+    @NotEmpty(message = "email is required")
     String email,
 
-    @NotNull(message = "password is required")
+    @NotEmpty(message = "password is required")
     String password,
 
-    @NotNull(message = "phoneNo is required")
+    @NotEmpty(message = "phoneNo is required")
     String phoneNo,
 
     String referralCode,
 
-    @NotNull(message = "emailOtp is required")
+    @NotEmpty(message = "emailOtp is required")
     String emailOtp,
 
-    @NotNull(message = "smsOtp is required")
+    @NotEmpty(message = "smsOtp is required")
     String smsOtp
 ) {
 }
