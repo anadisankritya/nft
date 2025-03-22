d121e5ac4e4145a6d97dee6d49080318326852f5
anadi.sankritya <anadi.sankritya@starhealth.in>
2025-03-22 22:42:52 +0530

    fix

--- src/main/java/com/nft/app/controller/UI/WalletRequestController.java
+++ src/main/java/com/nft/app/controller/UI/WalletRequestController.java
@@ -11,13 +11,11 @@ public class WalletRequestController {
 
   @GetMapping("/pending-withdrawal")
   public String showWithdrawRequestsPage() {
-    // The returned string corresponds to the name of the HTML file in templates (without the extension).
     return "pending_withdrawal";
   }
 
   @GetMapping("/pending-deposit")
   public String showDepositRequestsPage() {
-    // The returned string corresponds to the name of the HTML file in templates (without the extension).
     return "pending_deposit";
   }
 
